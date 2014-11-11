4fb773a096e818514977478dd9c721a571dc161e
yuki-sato.com <yuki@yuki-sato.com>
2014-11-11 21:04:02 +0900

    Initial Commit

---
+++ Sweety/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sweety
+//
+//  Created by oyabunn on 11/11/14.
+//  Copyright (c) 2014 yuki-sato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
